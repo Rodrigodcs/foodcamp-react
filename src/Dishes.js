83b0e99424c9a0284982f511e7ec60fc40ba5746
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2021-05-06 13:58:13 -0300

    Added new images

--- src/Dishes.js
+++ src/Dishes.js
@@ -3,30 +3,34 @@ import Section from "./Section"
 export default function Dishes(props){
     const serverData = {
         main:[
-            {index:0 ,name: "frango1",image:"images/Drinks/coquinha_gelada 1.png",price:13.50,description:["primeira","segunda"]},
-            {index:1 ,name: "frango2",image:"images/Drinks/coquinha_gelada 1.png",price:11.50,description:["primeira","segunda"]},
-            {index:2 ,name: "frango4",image:"images/Drinks/coquinha_gelada 1.png",price:11.50,description:["primeira","segunda"]},
-            {index:3 ,name: "frango3",image:"images/Drinks/coquinha_gelada 1.png",price:12.50,description:["primeira","segunda"]},
-            {index:4 ,name: "frangadasdasdo1",image:"images/Drinks/coquinha_gelada 1.png",price:10.50,description:["primsadeira","sesdadgunda"]}
+            {index:0 ,name: "Frango Yin Yang",image:"images/Main/frango_yin_yang.png",price:"32,00",description:["Acompanha arroz","Acompanha batata"]},
+            {index:1 ,name: "Feijoada",image:"images/Main/feijoada.jpg",price:"36,00",description:["Acompanha arroz","Acompanha farofa"]},
+            {index:2 ,name: "Panqueca",image:"images/Main/panqueca.jpg",price:"25,00",description:["Com muito molho","Quaijo ralado"]},
+            {index:3 ,name: "Lasagna",image:"images/Main/lasagna.jpeg",price:"28,00",description:["Bolognesa","Molho vermelho"]},
+            {index:4 ,name: "Sanduiche",image:"images/Main/sanduiche.jpg",price:"15,00",description:["Muita maionese","Muito presunto"]},
+            {index:5 ,name: "Spaghetti",image:"images/Main/spaghetti.jpg",price:"42,00",description:["Molho vermelho","Queijo ralado"]},
+            {index:6 ,name: "Bife a cavalo",image:"images/Main/bife_a_cavalo.jpg",price:"32,00",description:["Acompanha arroz","Acompanha farofa"]}
         ],
         drink:[
-            {index:0 ,name: "cocacola1",image:"images/Drinks/coquinha_gelada 1.png",price:13.50,description:["primeira","segunda"]},
-            {index:1 ,name: "cocacola2",image:"images/Drinks/coquinha_gelada 1.png",price:13.50,description:["primeira","segunda"]},
-            {index:2 ,name: "cocacola4",image:"images/Drinks/coquinha_gelada 1.png",price:13.50,description:["primeira","segunda"]},
-            {index:3 ,name: "cocacola3",image:"images/Drinks/coquinha_gelada 1.png",price:13.50,description:["primeira","segunda"]}
+            {index:0 ,name: "Coca-cola",image:"images/Drinks/coca_cola.png",price:"5,50",description:["Geladinha","Rodela de limão"]},
+            {index:1 ,name: "Chá Gelado",image:"images/Drinks/cha_gelado.png",price:"4,00",description:["Geladinho","Rodela de limão"]},
+            {index:2 ,name: "Tamarindo",image:"images/Drinks/tamarindo.jpg",price:"3,00",description:["Saboroso","Geladinho"]},
+            {index:3 ,name: "Tubaina",image:"images/Drinks/tubaina.jpg",price:"2,00",description:["Mais em conta","Vários sabores"]},
+            {index:4 ,name: "Água",image:"images/Drinks/agua.webp",price:"2,50",description:["Faz bem","Bem gelada"]}
         ],
         dessert:[
-            {index:0 ,name: "pudim99991",image:"images/Drinks/coquinha_gelada 1.png",price:13.50,description:["primeira","segunda"]},
-            {index:1 ,name: "pudim2",image:"images/Drinks/coquinha_gelada 1.png",price:13.50,description:["primeira","segunda"]},
-            {index:2 ,name: "pudim4",image:"images/Drinks/coquinha_gelada 1.png",price:13.50,description:["primeira","segunda"]},
-            {index:3 ,name: "pudim3",image:"images/Drinks/coquinha_gelada 1.png",price:13.50,description:["primeira","segunda"]}
+            {index:0 ,name: "Pudim",image:"images/Desserts/pudim.png",price:"13,30",description:["Bem docinho","Muito bom"]},
+            {index:1 ,name: "Gelatina",image:"images/Desserts/gelatina_mosaico.jpg",price:"7,40",description:["Deliciosa","Muito gostosa"]},
+            {index:2 ,name: "Churros",image:"images/Desserts/churros.jpg",price:"5,50",description:["Chocolate","Doce de Leite"]},
+            {index:3 ,name: "Bolo",image:"images/Desserts/bolo.jpg",price:"14,90",description:["Chocolate","Bem fofinho"]},
+            {index:4 ,name: "Musse",image:"images/Desserts/musse.jpg",price:"8,60",description:["Maracujá","Um pouco amargo"]}
         ],
     }
     return (
         <div class="dishes">
-            <Section dish={serverData.main} label="testando1" done={props.done} type="main"/>
-            <Section dish={serverData.drink} label="testando2" done={props.done} type="drink"/>
-            <Section dish={serverData.dessert} label="testando3"done={props.done} type="dessert"/>
+            <Section dish={serverData.main} label="Primeiro, seu prato" done={props.done} type="main"/>
+            <Section dish={serverData.drink} label="Agora, sua bebida" done={props.done} type="drink"/>
+            <Section dish={serverData.dessert} label="Por fim, sua sobremesa"done={props.done} type="dessert"/>
         </div>
     )
 }
